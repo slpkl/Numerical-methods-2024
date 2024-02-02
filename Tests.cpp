113f5ef937da3e4059b7c76fa07b7f121c662c82
Egor <57009978+trueuser3@users.noreply.github.com>
2024-02-03 00:00:04 +0300

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -1,56 +1,93 @@
-//#include "Exp.hpp"
-//#include "Consts.hpp"
+#include "Consts.hpp"
+#include "Exp.hpp"
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
-double Exp(double x){
-    return 1 + x+ x*x/2 + x*x*x/6 + x*x*x*x/24 + x*x*x*x*x/120 + x*x*x*x*x*x/720;
-}
+using namespace ADAAI;
 
-double my_abs(double a) {
-  return (a > 0)? a : (-1) * a;
-}
+const int N = 18;
+
+double my_abs(double a) { return (a > 0) ? a : (-1) * a; }
 
 int main() {
   double max_diff = 0;
-
+  int success_count = 0;
+  int unsuccess_count = 0;
 
   std::cout << "Check int:" << '\n';
   for (double i = -50; i <= 50; i++) {
     max_diff = std::max(max_diff, (my_abs(Exp(i) - exp(i))));
-    std::cout << "Diff: " << my_abs(Exp(i) - exp(i)) << " Pow = " << i <<'\n';
+    if (my_abs(Exp(i) - exp(i)) <= 1e-18) {
+      std::cout << "Test Passed, "
+                << "Pow = " << i << ", " << std::setprecision(N)
+                << "Our: " << Exp(i) << ", Ideal: " << exp(i)
+                << ", Diff: " << my_abs(Exp(i) - exp(i)) << '\n';
+      success_count++;
+    } else {
+      std::cout << "Test Failed, "
+                << "Pow = " << i << ", " << std::setprecision(N)
+                << "Our: " << Exp(i) << ", Ideal: " << exp(i)
+                << ", Diff: " << my_abs(Exp(i) - exp(i)) << '\n';
+      unsuccess_count++;
+    }
   }
 
   std::cout << '\n';
   std::cout << "Check float:" << '\n';
 
-  std::vector<float> float_arr = {
-      1.25,    1.5,   1.75, 2.25,  2.5,    2.75,   -1.25, -1.5,
-      -1.75,   -2.25, -2.5, -2.75, -25.5,  -24.5,  -23.5, -20.145,
-      -15.625, 25.5,  24.5, 23.5,  20.145, 15.625
-  };
+  std::vector<float> float_arr = {1.25,  1.5,   1.75,   2.25,    2.5,     2.75,
+                                  -1.25, -1.5,  -1.75,  -2.25,   -2.5,    -2.75,
+                                  -25.5, -24.5, -23.5,  -20.145, -15.625, 25.5,
+                                  24.5,  23.5,  20.145, 15.625};
 
   for (auto k : float_arr) {
     max_diff = std::max(max_diff, (my_abs(Exp(k) - exp(k))));
-    std::cout << "Diff: " << my_abs(Exp(k) - exp(k)) << " Pow = " << k <<'\n';
+    if (my_abs(Exp(k) - exp(k)) <= 1e-18) {
+      std::cout << "Test Passed, "
+                << "Pow = " << k << ", " << std::setprecision(N)
+                << "Our: " << Exp(k) << ", Ideal: " << exp(k)
+                << ", Diff: " << my_abs(Exp(k) - exp(k)) << '\n';
+      success_count++;
+    } else {
+      std::cout << "Test Failed, "
+                << "Pow = " << k << ", " << std::setprecision(N)
+                << "Our: " << Exp(k) << ", Ideal: " << exp(k)
+                << ", Diff: " << my_abs(Exp(k) - exp(k)) << '\n';
+      unsuccess_count++;
+    }
   }
 
   std::vector<double> double_arr = {
-      1.252525,    1.55555,   1.757575, 2.252525,  2.5555,    2.757575,   -1.252525, -1.52525,
-      -1.751313,   -2.251313, -2.51313, -2.751313, -25.51313,  -24.5515,  -23.52515, -20.14531,
-      -15.625544474, 25.585348,  24.505304, 23.584583,  20.1453123, 15.625543
-  };
+      1.252525,  1.55555,   1.757575,   2.252525,  2.5555,        2.757575,
+      -1.252525, -1.52525,  -1.751313,  -2.251313, -2.51313,      -2.751313,
+      -25.51313, -24.5515,  -23.52515,  -20.14531, -15.625544474, 25.585348,
+      24.505304, 23.584583, 20.1453123, 15.625543};
 
   std::cout << '\n';
   std::cout << "Check long double" << '\n';
 
   for (auto k : double_arr) {
     max_diff = std::max(max_diff, (my_abs(Exp(k) - exp(k))));
-    std::cout << "Diff: " << my_abs(Exp(k) - exp(k)) << " Pow = " << k <<'\n';
+    if (my_abs(Exp(k) - exp(k)) <= 1e-18) {
+      std::cout << "Test Passed, "
+                << "Pow = " << k << ", " << std::setprecision(N)
+                << "Our: " << Exp(k) << ", Ideal: " << exp(k)
+                << ", Diff: " << my_abs(Exp(k) - exp(k)) << '\n';
+      success_count++;
+    } else {
+      std::cout << "Test Failed, "
+                << "Pow = " << k << ", " << std::setprecision(N)
+                << "Our: " << Exp(k) << ", Ideal: " << exp(k)
+                << ", Diff: " << my_abs(Exp(k) - exp(k)) << '\n';
+      unsuccess_count++;
+    }
   }
-  
+
   std::cout << '\n';
-  std::cout << "Max diff:" << max_diff;
+  std::cout << "Max diff: " << max_diff << '\n';
+  std::cout << "Success: " << success_count << '\n';
+  std::cout << "Failed: " << unsuccess_count << '\n';
 }
