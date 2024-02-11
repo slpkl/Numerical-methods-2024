dc1c17fe023e4f3c6c630e607ee568c3cdd356d8
vlcir <112485129+vlcir@users.noreply.github.com>
2024-02-12 02:43:13 +0300

    Update Exp.hpp

--- Exp.hpp
+++ Exp.hpp
@@ -1,60 +1,85 @@
 #pragma once
-#include <cmath>
-#include <iostream>
-#include <limits>
-#include <type_traits>
 #include "Consts.hpp"
+#include <type_traits>
+#include <iostream>
+#include <cmath>
+#include <limits>
+
 
 namespace ADAAI {
 
-enum class MethodE : int { Taylor, Pade };
+enum class MethodE:int
+{
+    Taylor,
+    Pade
+};
 
 template <MethodE M = MethodE::Pade, typename F>
-constexpr F Exp(F x) {
+//template <typename F>
+constexpr F  Exp(F x){
     static_assert(std::is_floating_point_v<F>);
-    if (x != x) {
+    
+    if(x != x) {
         return x;
     }
-    F y_1;  // fractional part
-    F y_0;  // integer part
+    F y_1; //fractional part
+    F y_0; //integer part
     F y = x * Log2E<F>;
     y_1 = std::modf(y, &y_0);
     int64_t n;
 
-    if (y < 0) {
+    if (y < 0){
         if (y_1 < -0.5) {
             y_0--;
             y_1++;
         }
-    } else if (y_1 > 0.5) {
+    }
+    else if (y_1 > 0.5) {
         y_0++;
         y_1--;
     }
-
-    if (y_0 < INT64_MIN) {
+    
+    if(y_0 < INT64_MIN ){
         return 0.0;
-    } else if (y_0 > INT64_MAX) {
+    }
+    else if( y_0 > INT64_MAX ){
         return Inf<F>;
-    } else {
+    }
+    else {
         n = static_cast<int>(y_0);
     }
     F x_1 = y_1 / Log2E<F>;
-
-    if constexpr (M == MethodE::Taylor) {
-        F exp_temp = 1;
+    F exp_temp = 1;
+    if constexpr (M==MethodE::Taylor){
         int k = 1;
 
         F current_memb = 1;
-        while (k < 35) {
+        while(k < 20) {
             current_memb /= k;
             current_memb *= x_1;
             exp_temp += current_memb;
             k++;
         }
 
-        F exp_res = ldexpl(exp_temp, n);
-        return exp_res;
-    } else if constexpr (M == MethodE::Pade) {
+        }
+    else if constexpr(M==MethodE::Pade){
+        F numerator = 1;
+        F denumerator = 1;
+        F current_memb = 1;
+        for (int i = 0; i < 7; i++) {
+            current_memb *= x_1;
+            
+            numerator += current_memb / Polynom<F>[i];
+            if (i % 2 == 0) {
+                denumerator -= current_memb / Polynom<F>[i];
+            }
+            else {
+                denumerator += current_memb / Polynom<F>[i];
+            }
+        }
+        exp_temp = numerator / denumerator;
     }
+    F exp_res = ldexpl(exp_temp, n);  
+    return exp_res; 
 }
-}  // namespace ADAAI
+}
