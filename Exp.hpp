ce916ec6061950299600f4f7e1ed9eb83aa3fa69
Egor <57009978+trueuser3@users.noreply.github.com>
2024-02-12 02:18:38 +0300

    Update Exp.hpp

--- Exp.hpp
+++ Exp.hpp
@@ -1,71 +1,60 @@
 #pragma once
-#include "Consts.hpp"
-#include <type_traits>
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include <limits>
-
+#include <type_traits>
+#include "Consts.hpp"
 
 namespace ADAAI {
 
-
-enum class MethodE:int
-{
-    Taylor,
-    Pade
-};
+enum class MethodE : int { Taylor, Pade };
 
 template <MethodE M = MethodE::Pade, typename F>
-constexpr F  Exp(F x){
+constexpr F Exp(F x) {
     static_assert(std::is_floating_point_v<F>);
-    if(x != x) {
+    if (x != x) {
         return x;
     }
-    F y_1; //fractional part
-    F y_0; //integer part
+    F y_1;  // fractional part
+    F y_0;  // integer part
     F y = x * Log2E<F>;
     y_1 = std::modf(y, &y_0);
     int64_t n;
 
-    if (y < 0){
+    if (y < 0) {
         if (y_1 < -0.5) {
             y_0--;
             y_1++;
         }
-    }
-    else if (y_1 > 0.5) {
+    } else if (y_1 > 0.5) {
         y_0++;
         y_1--;
     }
-    
-    if(y_0 < INT64_MIN ){
+
+    if (y_0 < INT64_MIN) {
         return 0.0;
-    }
-    else if( y_0 > INT64_MAX ){
+    } else if (y_0 > INT64_MAX) {
         return Inf<F>;
-    }
-    else {
+    } else {
         n = static_cast<int>(y_0);
     }
     F x_1 = y_1 / Log2E<F>;
 
-    if constexpr (M==MethodE::Taylor){
+    if constexpr (M == MethodE::Taylor) {
         F exp_temp = 1;
         int k = 1;
 
         F current_memb = 1;
-        while(k < 35) {
+        while (k < 35) {
             current_memb /= k;
             current_memb *= x_1;
             exp_temp += current_memb;
             k++;
         }
 
-        F exp_res = ldexpl(exp_temp, n);  
-        return exp_res;            
-    }
-    else if constexpr(M==MethodE::Pade){
-
-    }
+        F exp_res = ldexpl(exp_temp, n);
+        return exp_res;
+    } else if constexpr (M == MethodE::Pade) {
     }
 }
+}  // namespace ADAAI
