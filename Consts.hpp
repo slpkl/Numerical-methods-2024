a882a6058c624e4fab59827369951d2cdfbf02c5
Egor <57009978+trueuser3@users.noreply.github.com>
2024-02-12 02:19:08 +0300

    Update Consts.hpp

--- Consts.hpp
+++ Consts.hpp
@@ -1,53 +1,53 @@
 #pragma once
+#include <cfloat>
 #include <cmath>
 #include <type_traits>
-#include <cfloat>
 
-namespace ADAAI
-{
+namespace ADAAI {
 
-    template <typename F>
-    constexpr inline F Log2E;
+template <typename F>
+constexpr inline F Log2E;
 
-    template <typename F>
-    constexpr inline F Eps;
+template <typename F>
+constexpr inline F Eps;
 
-    template <typename F>
-    constexpr inline F Sqrt2;
+template <typename F>
+constexpr inline F Sqrt2;
 
-    template <typename F>
-    constexpr inline F Inf;
+template <typename F>
+constexpr inline F Inf;
 
-    template <>
-    constexpr inline float Log2E<float> = M_LOG2Ef;
-    template <>
-    constexpr inline double Log2E<double> = M_LOG2E;
-    template <>
-    constexpr inline long double Log2E<long double> = M_LOG2El;
+template <>
+constexpr inline float Log2E<float> = M_LOG2Ef;
+template <>
+constexpr inline double Log2E<double> = M_LOG2E;
+template <>
+constexpr inline long double Log2E<long double> = M_LOG2El;
 
-    template <>
-    constexpr inline float Sqrt2<float> = M_SQRT2f;
-    template <>
-    constexpr inline double Sqrt2<double> = M_SQRT2;
-    template <>
-    constexpr inline long double Sqrt2<long double> = M_SQRT2l;
+template <>
+constexpr inline float Sqrt2<float> = M_SQRT2f;
+template <>
+constexpr inline double Sqrt2<double> = M_SQRT2;
+template <>
+constexpr inline long double Sqrt2<long double> = M_SQRT2l;
 
-    template <>
-    constexpr inline float Eps<float> = FLT_EPSILON;
-    template <>
-    constexpr inline double Eps<double> = DBL_EPSILON;
-    template <>
-    constexpr inline long double Eps<long double> = LDBL_EPSILON;
+template <>
+constexpr inline float Eps<float> = FLT_EPSILON;
+template <>
+constexpr inline double Eps<double> = DBL_EPSILON;
+template <>
+constexpr inline long double Eps<long double> = LDBL_EPSILON;
 
-    template <>
-    constexpr inline float Inf<float> = HUGE_VALF;
-    template <>
-    constexpr inline double Inf<double> = HUGE_VAL;
-    template <>
-    constexpr inline long double Inf<long double> = HUGE_VALL;
+template <>
+constexpr inline float Inf<float> = HUGE_VALF;
+template <>
+constexpr inline double Inf<double> = HUGE_VAL;
+template <>
+constexpr inline long double Inf<long double> = HUGE_VALL;
 
-    struct SelectArray {
-    static constexpr inline std::array<long double, 3> Array = {1.0L, 2.0L, 3.0L};
+struct SelectArray {
+    static constexpr inline std::array<long double, 3> Array = {
+        1.0L, 2.0L, 3.0L};
 };
 
 template <>
@@ -62,7 +62,8 @@ struct SelectArray<double> {
 
 template <>
 struct SelectArray<long double> {
-    static constexpr inline std::array<long double, 3> Array = {1.0L, 2.0L, 3.0L};
+    static constexpr inline std::array<long double, 3> Array = {
+        1.0L, 2.0L, 3.0L};
 };
 
-}
+}  // namespace ADAAI
