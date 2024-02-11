8c26d305d5a486814df3e31638cabec41656f470
vlcir <112485129+vlcir@users.noreply.github.com>
2024-02-12 02:23:23 +0300

    Update Consts.hpp

--- Consts.hpp
+++ Consts.hpp
@@ -1,69 +1,61 @@
 #pragma once
-#include <cfloat>
 #include <cmath>
 #include <type_traits>
+#include <cfloat>
+#include <array>
 
 namespace ADAAI {
 
-template <typename F>
+template < typename F >
 constexpr inline F Log2E;
 
-template <typename F>
+template < typename F >
 constexpr inline F Eps;
 
-template <typename F>
+template < typename F >
 constexpr inline F Sqrt2;
 
-template <typename F>
+template < typename F >
 constexpr inline F Inf;
 
-template <>
-constexpr inline float Log2E<float> = M_LOG2Ef;
-template <>
-constexpr inline double Log2E<double> = M_LOG2E;
-template <>
-constexpr inline long double Log2E<long double> = M_LOG2El;
+template < typename F >
+constexpr inline std::array<F, 7> Polynom;
 
 template <>
-constexpr inline float Sqrt2<float> = M_SQRT2f;
+constexpr inline float Log2E <float> = M_LOG2Ef;
 template <>
-constexpr inline double Sqrt2<double> = M_SQRT2;
+constexpr inline double Log2E <double> = M_LOG2E;
 template <>
-constexpr inline long double Sqrt2<long double> = M_SQRT2l;
+constexpr inline long double Log2E  <long double> = M_LOG2El;
 
 template <>
-constexpr inline float Eps<float> = FLT_EPSILON;
+constexpr inline float Sqrt2 <float> = M_SQRT2f;
 template <>
-constexpr inline double Eps<double> = DBL_EPSILON;
+constexpr inline double Sqrt2 <double> = M_SQRT2;
 template <>
-constexpr inline long double Eps<long double> = LDBL_EPSILON;
+constexpr inline long double Sqrt2 <long double> = M_SQRT2l;
 
 template <>
-constexpr inline float Inf<float> = HUGE_VALF;
+constexpr inline float Eps <float> = FLT_EPSILON;
 template <>
-constexpr inline double Inf<double> = HUGE_VAL;
+constexpr inline double Eps <double> = DBL_EPSILON;
 template <>
-constexpr inline long double Inf<long double> = HUGE_VALL;
-
-struct SelectArray {
-    static constexpr inline std::array<long double, 3> Array = {
-        1.0L, 2.0L, 3.0L};
-};
+constexpr inline long double Eps  <long double> = LDBL_EPSILON;
 
 template <>
-struct SelectArray<float> {
-    static constexpr inline std::array<float, 3> Array = {1.0f, 2.0f, 3.0f};
-};
+constexpr inline float Inf <float> = HUGE_VALF;
+template <>
+constexpr inline double Inf <double> = HUGE_VAL;
+template <>
+constexpr inline long double Inf  <long double> = HUGE_VALL;
+
+
 
 template <>
-struct SelectArray<double> {
-    static constexpr inline std::array<double, 3> Array = {1.0, 2.0, 3.0};
-};
-
+constexpr inline std::array<float, 7> Polynom <std::array<float, 7>> = {0.008333333333333333333333333333, 0.1, 0.5, 0., 0., 0., 0.};
 template <>
-struct SelectArray<long double> {
-    static constexpr inline std::array<long double, 3> Array = {
-        1.0L, 2.0L, 3.0L};
-};
+constexpr inline std::array<double, 7> Polynom <std::array<double, 7>> = {0.008333333333333333333333333333, 0.1, 0.5, 0., 0., 0., 0.};
+template <>
+constexpr inline long double Inf  <long double> = HUGE_VALL;
 
-}  // namespace ADAAI
+}
