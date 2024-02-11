1450c762102570108f37eb454c0143f228ebbdc2
vlcir <112485129+vlcir@users.noreply.github.com>
2024-02-12 00:18:24 +0300

    Update Exp.hpp

--- Exp.hpp
+++ Exp.hpp
@@ -18,16 +18,7 @@ constexpr F Pow(F x, int k) {
 }
 
 template <typename F>
-constexpr F AntiFactorial(F x_pow, int n) {
-    F res = x_pow;
-    for (int64_t i = 1; i <= n; i++) {
-        res = res / static_cast<F>(i);
-    }
-    return res;
-}
-
-template <typename F>
-constexpr F Exp(F x){
+constexpr F  Exp(F x){
     static_assert(std::is_floating_point_v<F>);
     if(x != x) {
         return x;
@@ -59,40 +50,18 @@ constexpr F Exp(F x){
         n = static_cast<int>(y_0);
     }
     F x_1 = y_1 / Log2E<F>;
-    F exp_temp = 0;
-    int k = 0;
-    while(k < 25) {
-        F current_memb = AntiFactorial(Pow(x_1, k), k);
+    F exp_temp = 1;
+    int k = 1;
+
+    F current_memb = 1;
+    while(k < 35) {
+        current_memb /= k;
+        current_memb *= x_1;
         exp_temp += current_memb;
-
-        if(x_1 > 0) {
-            F res_error = Sqrt2<F> * AntiFactorial(Pow(x_1, k+1), k+1);
-            if (res_error < 10 * Eps<F>) {
-                break;
-            }
-        }
-        else {
-            F res_error = Pow(x_1, k + 1) / AntiFactorial(Pow(x_1, k+1), k+1);
-
-            if (res_error > 0) {
-                if (res_error < 10 * Eps<F>) {
-                break;
-                }
-            }
-            else {
-                if (-res_error < 10 * Eps<F>) {
-                break;
-                }
-            }
-        }
-
         k++;
     }
 
-    F exp_res = ldexp(exp_temp, n);
-    
+    F exp_res = ldexp(exp_temp, n);  
     return exp_res;    
-
     }
-
 }
