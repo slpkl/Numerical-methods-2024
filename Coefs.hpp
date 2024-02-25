21eacb100cd1cdc66439d7b21e1be6b26585757c
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-02-25 17:30:53 +0300

    Update Coefs.hpp
    accuracy impoved

--- Coefs.hpp
+++ Coefs.hpp
@@ -7,7 +7,9 @@ void Coefs(int N, gsl_vector *c) {
     gsl_vector *b = gsl_vector_alloc(N + 1);
 
     gsl_matrix_set_all(A, 0.0l);
-    gsl_vector_set_all(b, 0.0l);
+
+    gsl_vector_set_zero(b);
+    
     gsl_vector_set(b, N, 1.0l);
 
     for(int k = 0; k < N; k++) {
@@ -16,29 +18,30 @@ void Coefs(int N, gsl_vector *c) {
             if (n % 2 == 0) {
                 if (k % 2 != 0) {
                     if (k == 1) {
-                        gsl_matrix_set(A, k, n, static_cast<double>(n));
-                    }
-                    else {
                         gsl_matrix_set(A, k, n, static_cast<double>(2 * n));
-                    } 
-                }
-                else {
-                    if (k == 0) {
-                        gsl_matrix_set(A, k, n, static_cast<double>(n));
                     }
                     else {
                         gsl_matrix_set(A, k, n, static_cast<double>(2 * n));
                     } 
                 }
             }
-
+            else if (n % 2 == 1) {
+                    if (k == 0) {
+                        gsl_matrix_set(A, k, n, static_cast<double>(n));
+                    }
+                    else if (k % 2 == 0){
+                        gsl_matrix_set(A, k, n, static_cast<double>(2 * n));
+                    } 
+            }
             gsl_matrix_set(A, k, k, -1.0l);
         }
     }
 
+    //Add Sum_0 ^N c_n * T_n(0) = 1 equality
+
     for(int n = 0; n < N+1; n++) {
         if(n % 2 == 0) {
-            if((n / 2) % 2 == 0) {
+            if(n % 4 == 0) {
                 gsl_matrix_set(A, N, n, static_cast<double>(1));
             }
             else {
@@ -49,6 +52,7 @@ void Coefs(int N, gsl_vector *c) {
 
 
     gsl_linalg_HH_solve(A, b, c);
+
     gsl_matrix_free(A);
     gsl_vector_free(b);
 
