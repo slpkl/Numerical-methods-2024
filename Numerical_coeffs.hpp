a1c43f4420db49ec13fc2dead4df5e20874aab49
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-03-07 04:21:26 +0300

    Create Numerical_coeffs.hpp

---
+++ Numerical_coeffs.hpp
@@ -0,0 +1,14 @@
+#include <cmath>
+
+template <typename F>
+constexpr F numerical_coefs(int N, int k) {
+  int m = N + 1;
+  F result_sum = 2 / (N + 1);
+  for (int i = 1; i <= N + 1; i++) {
+    F argument = std::cos((2 * i - 1) / (2 * N + 2));
+    F exp_pow_acos = std::exp(acos(argument));
+    F T_k = Tn(k, argument);
+    result_sum = result_sum * exp_pow_acos * T_k;
+    return result_sum;
+  }
+}
