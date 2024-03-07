a90142f846b999429d11df468faa32ed73560e1f
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-03-07 04:38:26 +0300

    Update Numerical_coeffs.hpp

--- Numerical_coeffs.hpp
+++ Numerical_coeffs.hpp
@@ -1,17 +1,21 @@
 #include <cmath>
 #include "Chebyshev.hpp"
 #include <iostream>
-#include <Consts.hpp>
+#include "Consts.hpp"
+
+using namespace ADAAI;
 
 template <typename F>
 constexpr F numerical_coefs(int N, int k) {
   int m = N + 1;
-  F result_sum = 2 / (N + 1);
+  F result_sum = 0;
   for (int i = 1; i <= N + 1; i++) {
     F argument = std::cos((2 * i - 1) / (2 * N + 2) * Pi<F>);
     F exp_pow_acos = std::exp(std::acos(argument));
     F T_k = Tn(k, argument);
-    result_sum = result_sum * exp_pow_acos * T_k;
+    result_sum +=  exp_pow_acos * T_k;
   }
+  result_sum *=  2 / (N + 1);
   return result_sum;
 }
+
