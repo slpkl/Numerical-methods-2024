5db9b720b95d4ef4275189676c6ada286098b0c3
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-03-07 04:24:44 +0300

    Update Numerical_coeffs.hpp

--- Numerical_coeffs.hpp
+++ Numerical_coeffs.hpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include "Chebyshev.hpp"
 
 template <typename F>
 constexpr F numerical_coefs(int N, int k) {
@@ -6,7 +7,7 @@ constexpr F numerical_coefs(int N, int k) {
   F result_sum = 2 / (N + 1);
   for (int i = 1; i <= N + 1; i++) {
     F argument = std::cos((2 * i - 1) / (2 * N + 2));
-    F exp_pow_acos = std::exp(acos(argument));
+    F exp_pow_acos = std::exp(std::acos(argument));
     F T_k = Tn(k, argument);
     result_sum = result_sum * exp_pow_acos * T_k;
     return result_sum;
