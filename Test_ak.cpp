7e5f75408bdcd1b96839b4358b572b22408c257a
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-03-09 21:22:10 +0300

    Update Test_ak.cpp

--- Test_ak.cpp
+++ Test_ak.cpp
@@ -1,31 +1,24 @@
-#include "Consts.hpp"
-#include "Numerical_coeffs.hpp"
-#include "Chebyshev.hpp"
-#include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <vector>
-#include <iomanip>
+#include "Chebyshev.hpp"
+#include "Consts.hpp"
 
-using namespace ADAAI;
-
-const int N = 52;
+namespace ADAAI {
 
 template <typename F>
-constexpr F a_k(int k) {
-  return (M_2_PI * (cos(Pi<F> * k) * exp(Pi<F>) - 1)) / (k*k + 1);
+constexpr F numerical_coefs(int N, int k) {
+  int m = N + 1;
+  F result_sum = 0;
+  for (int i = 1; i <= N + 1; i++) {
+    F argument = std::cos(
+        Pi<F> * (2 * static_cast<F>(i) - 1) / (2 * static_cast<F>(N) + 2)
+    );
+    F exp_pow_acos = std::exp(std::acos(argument));
+    F T_k = Tn(k, argument);
+    result_sum += exp_pow_acos * T_k;
+  }
+  result_sum = result_sum / (N + 1);
+  result_sum = result_sum * 2;
+  return result_sum;
 }
-
-long double my_abs(long double x) {
-    return (x > 0) ? x : -x;
-}
-
-int main() {
-    for (int k = 1; k < 50; k++) {
-        std::cout << "k = " << k << ", " << std::setprecision(15) << "Analyst: " << std::setprecision(15)
-        << a_k<long double>(k) << ", " 
-        << "Numerical: " << numerical_coefs<long double>(N, k)
-        << ", " << "Diff: " << my_abs(a_k<long double>(k) - numerical_coefs<long double>(N, k)) 
-        << '\n';
-    }
-}
+}  // namespace ADAAI
