6ab145a7403ec0738fed8550764b59dce60b7b40
vlcir <112485129+vlcir@users.noreply.github.com>
2024-03-09 21:05:23 +0300

    Update Numerical_coeffs.hpp

--- Numerical_coeffs.hpp
+++ Numerical_coeffs.hpp
@@ -10,7 +10,7 @@ constexpr F numerical_coefs(int N, int k) {
   int m = N + 1;
   F result_sum = 0;
   for (int i = 1; i <= N + 1; i++) {
-    F argument = std::cos((2 * i - 1) / (2 * N + 2) * Pi<F>);
+    F argument = std::cos(Pi<F> * (2 * static_cast<F>(i) - 1) / (2 * static_cast<F>(N) + 2));
     F exp_pow_acos = std::exp(std::acos(argument));
     F T_k = Tn(k, argument);
     result_sum +=  exp_pow_acos * T_k;
@@ -19,4 +19,3 @@ constexpr F numerical_coefs(int N, int k) {
   result_sum = result_sum * 2;
   return result_sum;
 }
-
