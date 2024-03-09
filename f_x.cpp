ccb6baa1f7ed0a1c0f77a125f560dc7da671c2f3
Egor <57009978+trueuser3@users.noreply.github.com>
2024-03-09 22:42:33 +0300

    Create f_x.cpp

---
+++ f_x.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "Consts.hpp"
+#include <cmath>
+#include "Numerical_coeffs.hpp"
+
+using namespace ADAAI;
+
+const int N = 20;
+const int num_N = 1e5;
+
+long double coeffs_arr[N];
+long double x_i[N];
+
+void x_calc(){
+    for (int i = 0; i < N; i++) {
+        x_i[i] = (Pi<long double> * i) / (N + 1);
+    }
+}
+
+void coeffs() {
+    for (int i = 0; i < N; i++) {
+        coeffs_arr[i] = numerical_coefs<long double>(num_N, i);
+    }
+}
+
+long double f(long double x) {
+    coeffs();
+    long double function_res = coeffs_arr[0] / 2;
+    for (int i = 1; i < N; i++) {
+        function_res += coeffs_arr[i] * cos(i * x);
+    }
+    return function_res;
+}
+
+int main() {
+    x_calc();
+    for (int i = 0; i < N; i++) {
+        std::cout << "x_i = " << x_i[i] << ", " << "f(x) = " << f(x_i[i]) << '\n';
+    }
+}
