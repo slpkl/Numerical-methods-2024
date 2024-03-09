8a7ca9a2781b67bd3e996440da21b26f82db2ffd
Egor <57009978+trueuser3@users.noreply.github.com>
2024-03-09 21:10:37 +0300

    Update Test_ak.cpp

--- Test_ak.cpp
+++ Test_ak.cpp
@@ -9,6 +9,8 @@
 
 using namespace ADAAI;
 
+const int N = 52;
+
 template <typename F>
 constexpr F a_k(int k) {
   return (M_2_PI * (cos(Pi<F> * k) * exp(Pi<F>) - 1)) / (k*k + 1);
@@ -19,11 +21,11 @@ long double my_abs(long double x) {
 }
 
 int main() {
-    for (int k = 0; k < 50; k++) {
+    for (int k = 1; k < 50; k++) {
         std::cout << "k = " << k << ", " << std::setprecision(15) << "Analyst: " << std::setprecision(15)
         << a_k<long double>(k) << ", " 
-        << "Numerical: " << numerical_coefs<long double>(10, k)
-        << ", " << "Diff: " << my_abs(a_k<long double>(k) - numerical_coefs<long double>(10, k)) 
+        << "Numerical: " << numerical_coefs<long double>(N, k)
+        << ", " << "Diff: " << my_abs(a_k<long double>(k) - numerical_coefs<long double>(N, k)) 
         << '\n';
     }
 }
