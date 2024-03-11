112575369fec9b0f41e0fdb0907f6227d309b83a
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-03-11 03:29:52 +0300

    Update Differentiation.hpp

--- Differentiation.hpp
+++ Differentiation.hpp
@@ -23,14 +23,11 @@ enum class WhichD : int { x, y, xx, yy, xy };
 
 const double h = 1e-4;
 
-template <
-    WhichD W = WhichD::xx,
-    DiffMethod M = DiffMethod::stencil3,
-    typename Collable>
+template <WhichD W, DiffMethod M, typename Collable>
 constexpr double Differentiator(const Collable &F, double x, double y) {
+  double h_x = my_abs(x) < 1 ? h : h * my_abs(x);
+  double h_y = my_abs(y) < 1 ? h : h * my_abs(x);
   if constexpr (M == DiffMethod::stencil3) {
-    double h_x = my_abs(x) < 1 ? h : h * my_abs(x);
-    double h_y = my_abs(y) < 1 ? h : h * my_abs(x);
     if constexpr (W == WhichD::x) {
       return (F(x + h, y) - F(x - h, y)) / (2 * h);
     } else if constexpr (W == WhichD::y) {
@@ -46,10 +43,27 @@ constexpr double Differentiator(const Collable &F, double x, double y) {
              (F(x + h_x, y + h_y) - F(x - h_x, y - h_y) - F(x + h_x, y - h_y) +
               F(x - h_x, y - h_y));
     }
-
   } else if constexpr (M == DiffMethod::stencil3Extra) {
   } else if constexpr (M == DiffMethod::stencil5) {
-  } else if constexpr (M == DiffMethod::stencil5Extra) {
+    if constexpr (W == WhichD::x) {
+      return (1 / 12) * F(x - 2 * h_x, y) - (2 / 3) * F(x - h_x, y) +
+             (2 / 3) * F(x + h_x, y) - (1 / 12) * F(x + 2 * h_x, y);
+    } else if constexpr (W == WhichD::y) {
+      return (1 / 12) * F(x, y - 2 * h_y) - (2 / 3) * F(x, y - h_y) +
+             (2 / 3) * F(x, y + h_y) - (1 / 12) * F(x, y + 2 * h_y);
+    } else if constexpr (W == WhichD::xx) {
+      return (-1 / 12) * F(x - 2 * h_x, y) + (4 / 3) * F(x - h_x, y) -
+             (5 / 2) * F(x, y) + (4 / 3) * F(x + h_x, y) -
+             (1 / 12) * F(x + 2 * h_x, y);
+    } else if constexpr (W == WhichD::yy) {
+      return (-1 / 12) * F(x, y - 2 * h_y) + (4 / 3) * F(x, y - h_y) -
+             (5 / 2) * F(x, y) + (4 / 3) * F(x, y + h_y) -
+             (1 / 12) * F(x, y + 2 * h_y);
+    } else if constexpr (W == WhichD::xy) {
+    }
+  }
+
+  else if constexpr (M == DiffMethod::stencil5Extra) {
   } else if constexpr (M == DiffMethod::FwdAAD) {
   }
 }
