272c3674792a5300290d953db285797ff1d6de4b
Emelyanova Anastasia <112266214+slpkl@users.noreply.github.com>
2024-06-11 03:54:55 +0300

    code style small fixes

--- RHS.hpp
+++ RHS.hpp
@@ -11,14 +11,29 @@ class RHS
 
 private:
   // fields are coordinates of our vector u - initial state
-  double x;
-  double v_x;
-  double y;
-  double v_y;
+  double x = 0;
+  double v_x = 0;
+  double y = 0;
+  double v_y = 0;
+
+  // memoised constants
+  const double m = 106.0;            // kg - projectile mass
+  const double d = 0.216;            // m - diameter
+  const double v_0 = 1640;           // m/sec - initial velocity
+  const double pi = 3.1415926535897; // pi constant
+  const double S = pi * d * d / 4;   // projectile cross-section
 
 public:
+  constexpr RHS(double p_x, double p_v_x, double p_y, double p_v_y)
+  {
+    x = p_x;
+    v_x = p_v_x;
+    y = p_y;
+    v_y = p_v_y;
+  }
+
   // compute Air speed via height
-  double compute_A(double h)
+  constexpr double compute_A(double h)
   {
     StandartAtmosphereModel atmosphere(h);
     double p = atmosphere.getPressure();
@@ -27,7 +42,7 @@ public:
   }
 
   // compute Mach number
-  double compute_M(double v, double h)
+  constexpr double compute_M(double v, double h)
   {
     double A_h = compute_A(h);
     return v / A_h;
@@ -46,8 +61,8 @@ public:
   }
 
   // finally, compute new vector u*
-  std::tuple<double, double, double, double> compute_dt_u(double x, double v_x,
-                                                          double y, double v_y)
+  constexpr std::tuple<double, double, double, double>
+  compute_dt_u(double x, double v_x, double y, double v_y)
   {
     double v = std::sqrt(v_x * v_x + v_y * v_y);
     double M = compute_M(v, y);
@@ -59,24 +74,8 @@ public:
     return std::make_tuple(x_new, vx_new, y_new, vy_new);
   }
 
-  RHS(double p_x, double p_v_x, double p_y, double p_v_y)
-  {
-    x = p_x;
-    v_x = p_v_x;
-    y = p_y;
-    v_y = p_v_y;
-  }
-
-  std::tuple<double, double, double, double> getRHS()
+  const std::tuple<double, double, double, double> getRHS()
   {
     return compute_dt_u(x, v_x, y, v_y);
   }
-
-  // memoised constants
-
-  static constexpr double m = 106.0;            // kg - projectile mass
-  static constexpr double d = 0.216;            // m - diameter
-  static constexpr double v_0 = 1640;           // m/sec - initial velocity
-  static constexpr double pi = 3.1415926535897; // pi constant
-  static constexpr double S = pi * d * d / 4;   // projectile cross-section
 };
